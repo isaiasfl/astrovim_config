7df548d0a5ccf1f2fbe09855c1a765866d5dca2e
Isaías FL <ifernandez@ieshlanz.esgit config --global user.name isaiasfl>
2022-08-08 00:52:50 +0200

    Mejoras3

--- init.lua
+++ init.lua
@@ -1,5 +1,37 @@
 return {
- -- --------------------- función para obligar a llamar el menu al cerrar el buffer ---------------
+
+ -- -------------- Tema NVChad --
+
+ default_theme = {
+    colors = function(C)
+      C.telescope_green = C.green
+      C.telescope_red = C.red
+      C.telescope_fg = C.fg
+      C.telescope_bg = C.black_1
+      C.telescope_bg_alt = C.bg_1
+      return C
+    end,
+    highlights = function(hl)
+      local C = require "default_theme.colors"
+      hl.TelescopeBorder = { fg = C.telescope_bg_alt, bg = C.telescope_bg }
+      hl.TelescopeNormal = { bg = C.telescope_bg }
+      hl.TelescopePreviewBorder = { fg = C.telescope_bg, bg = C.telescope_bg }
+      hl.TelescopePreviewNormal = { bg = C.telescope_bg }
+      hl.TelescopePreviewTitle = { fg = C.telescope_bg, bg = C.telescope_green }
+      hl.TelescopePromptBorder = { fg = C.telescope_bg_alt, bg = C.telescope_bg_alt }
+      hl.TelescopePromptNormal = { fg = C.telescope_fg, bg = C.telescope_bg_alt }
+      hl.TelescopePromptPrefix = { fg = C.telescope_red, bg = C.telescope_bg_alt }
+      hl.TelescopePromptTitle = { fg = C.telescope_bg, bg = C.telescope_red }
+      hl.TelescopeResultsBorder = { fg = C.telescope_bg, bg = C.telescope_bg }
+      hl.TelescopeResultsNormal = { bg = C.telescope_bg }
+      hl.TelescopeResultsTitle = { fg = C.telescope_bg, bg = C.telescope_bg }
+      return hl
+    end,
+  },
+
+
+
+-- -- Configuración para que cuando se cierre el último buffer se abra de nuevo el menú siempre ----
   polish = function()
     local function alpha_on_bye(cmd)
       local bufs = vim.fn.getbufinfo { buflisted = true }
@@ -19,4 +51,6 @@ return {
       end, { desc = "Close buffer" })
     end
   end,
+
+
 }
